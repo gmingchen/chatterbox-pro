59f3bc3d00a8f85421cd0de6441461e23c4f62c5
gumingchen <1240235512@qq.com>
2024-06-07 17:33:56 +0800

    update

--- src/main/java/com/slipper/modules/apply/model/req/ApplyReviewFriendReqVO.java
+++ src/main/java/com/slipper/modules/apply/model/req/ApplyReviewFriendReqVO.java
@@ -15,11 +15,11 @@ public class ApplyReviewFriendReqVO extends ApplyReviewReqVO {
     /**
      * 分组ID
      */
-    @NotNull(message = "分组ID不能为空")
+//    @NotNull(message = "分组ID不能为空")
     private Long groupingId;
     /**
      * 备注
      */
-    @Length(max = 50, message = "备注长度不能超过50个字符")
+//    @Length(max = 50, message = "备注长度不能超过50个字符")
     private String remark;
 }
